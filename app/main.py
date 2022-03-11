65b13bcbb77b0cd551153ab9bf411291d23873a0
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-11 16:12:12 -0500

    new test

--- app/main.py
+++ app/main.py
@@ -3,5 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1>Welcome to the secure Fingerprint Voting system!</h1>"
     return "<h1>Testing... Testing.. 1..2...3...!</h1>"
