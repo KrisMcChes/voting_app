409547221f027d159440ebe757b96d27ef3f333b
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-11 22:43:15 -0500

    more test

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1>Testing... Testing.. 1..2...3...!</h1>"
-
-def home():
 	   return render_template('home.html')
 if __name__ == '__main__':
    app.run()
+
+  # def index():
+   # return "<h1>Testing... Testing.. 1..2...3...!</h1>"
