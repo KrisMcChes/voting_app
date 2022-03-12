2bb5df999f0d24750620cbbe39962ef8dcc6da4d
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-12 00:48:40 -0500

    current stable version, trying to get login page

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,7 +6,7 @@ def homepage():
     return """
 <!DOCTYPE html>
 <head>
-   <title>My title</title>
+   <title>Voting Application</title>
    <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
 </head>
 <body style="width: 880px; margin: auto;">  
@@ -14,6 +14,17 @@ def homepage():
     <p>Login:</p>
 </body>
 """
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
