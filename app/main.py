6cae7d04a1c167d023861606df2779ff2cc2b9d8
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-11 22:40:59 -0500

    hosting html test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<h1>Second Test</h1>"
+    return "<h1>Testing... Testing.. 1..2...3...!</h1>"
 
+def home():
+	   return render_template('home.html')
+if __name__ == '__main__':
+   app.run()
