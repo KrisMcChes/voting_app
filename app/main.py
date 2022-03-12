026338d286997a38ba67940172e93b7c31fc86ae
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-12 00:41:33 -0500

    figured out basic html and CDD with flask

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
-app = Flask(__name__,
-            template_folder="templates")
+from flask import Flask
+app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route('/')
+def homepage():
+    return """
+<!DOCTYPE html>
+<head>
+   <title>My title</title>
+   <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
+</head>
+<body style="width: 880px; margin: auto;">  
+    <h1>Voting App Homepage</h1>
+    <p>Login:</p>
+</body>
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
