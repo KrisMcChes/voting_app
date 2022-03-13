0995bed91d474af2aa67753328301ed9711c3934
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-12 19:40:03 -0500

    still working on getting templates to work

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-app = Flask(__name__)
+app = Flask(__name__, template_folder="iCloud Drive/Desktop/voting_app/app/templates")
 
 @app.route('/')
 def homepage():
@@ -15,16 +15,21 @@ def homepage():
 </body>
 """
 
-# Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+
+# # Route for handling the login page logic
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+#             error = 'Invalid Credentials. Please try again.'
+#         else:
+#             return redirect(url_for('home'))
+#     return render_template('iCloud Drive/Desktop/voting_app/app/templates/login.html', error=error)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
