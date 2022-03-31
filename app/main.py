55964993c16a45be12ba872b28c7a8b1ed5a94a1
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-03-31 16:04:11 -0400

    Working on database

--- app/main.py
+++ app/main.py
@@ -1,23 +1,32 @@
 from flask import Flask, render_template, redirect, url_for, request
+# from models import db
 app = Flask(__name__, template_folder="templates")
 
-@app.route('/')
+
+# # load config from the config file we created earlier 
+# app.config.from_object('config')
+
+# # initialize and create the database 
+# db.init_app(app)
+# db.create_all(app=app)
+
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return """
-<!DOCTYPE html>
-<head>
-   <title>Voting Application</title>
-   <link rel="stylesheet" href="http://stash.compjour.org/assets/css/foundation.css">
-</head>
-<body style="width: 880px; margin: auto;">  
-    <h1>Voting App Homepage</h1>
-    <p>Login:</p>
-</body>
-"""
+   return render_template('homepage.html')  # render a template
+
 
 @app.route('/welcome', methods=['GET', 'POST'])
 def welcome():
-    return render_template('welcome.html')  # render a template
+    #    if request.method == 'POST':
+    #     if request.form.get('action1') == 'VALUE1':
+    #         pass # do something
+    #     elif  request.form.get('action2') == 'VALUE2':
+    #         pass # do something else
+    #     else:
+    #         pass # unknown
+    #    elif request.method == 'GET':
+    #     return render_template('welcome.html', form=form)
+       return render_template('welcome.html')  # render a template
 
 
 # Route for handling the login page logic
