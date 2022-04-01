30754df076796ad2bc2844febe9843b6902c6076
Shepard-Jeffrey <JS3968a@student.american.edu>
2022-04-01 04:54:04 -0400

    Error with important libraries?

--- app/main.py
+++ app/main.py
@@ -1,36 +1,36 @@
 from flask import Flask, render_template, redirect, url_for, request
-# from models import db
-app = Flask(__name__, template_folder="templates")
+from models import db
+voteapp = Flask(__name__, template_folder="templates")
 
 
-# # load config from the config file we created earlier 
-# app.config.from_object('config')
+# load config from the config file we created earlier 
+voteapp.config.from_object('config')
 
-# # initialize and create the database 
-# db.init_app(app)
-# db.create_all(app=app)
+# initialize and create the database 
+db.init_app(voteapp)
+db.create_all(app=voteapp)
 
-@app.route('/', methods=['GET', 'POST'])
+@voteapp.route('/', methods=['GET', 'POST'])
 def homepage():
    return render_template('homepage.html')  # render a template
 
 
-@app.route('/welcome', methods=['GET', 'POST'])
+@voteapp.route('/welcome', methods=['GET', 'POST'])
 def welcome():
-    #    if request.method == 'POST':
-    #     if request.form.get('action1') == 'VALUE1':
-    #         pass # do something
-    #     elif  request.form.get('action2') == 'VALUE2':
-    #         pass # do something else
-    #     else:
-    #         pass # unknown
-    #    elif request.method == 'GET':
-    #     return render_template('welcome.html', form=form)
+       if request.method == 'POST':
+        if request.form.get('action1') == 'VALUE1':
+            pass # do something
+        elif  request.form.get('action2') == 'VALUE2':
+            pass # do something else
+        else:
+            pass # unknown
+       elif request.method == 'GET':
+        return render_template('welcome.html', form=form)
        return render_template('welcome.html')  # render a template
 
 
 # Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@voteapp.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -41,4 +41,4 @@ def login():
     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    voteapp.run(debug=True, use_reloader=True)
